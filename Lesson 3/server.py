85ddb17de16093b4a5ad5d9add890124ade37df3
Амир <Smiker1@yandex.ru>
2022-12-02 16:02:22 +0300

    Tasks

--- Lesson 3/server.py
+++ Lesson 3/server.py
@@ -22,24 +22,30 @@ def message_response(message):
         return json.dumps(response).encode('utf-8')
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    try:
-        addr = sys.argv[1]
-    except IndexError:
-        addr = 'localhost'
-    try:
-        port = int(sys.argv[2])
-    except IndexError:
-        port = 7777
-    except ValueError:
-        sys.exit('Порт не числовое значение')
+def server_main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        try:
+            addr = sys.argv[1]
+        except IndexError:
+            addr = 'localhost'
+        try:
+            port = int(sys.argv[2])
+        except IndexError:
+            port = 7777
+        except ValueError:
+            sys.exit('Порт не числовое значение')
 
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((addr, port))
-    server.listen(1)
-    while True:
-        client, addr = server.accept()
-        message = client.recv(9999)
-        print(message)
-        response = message_response(message)
-        client.send(response)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind((addr, port))
+        server.listen(1)
+        while True:
+            client, addr = server.accept()
+            message = client.recv(9999)
+            print(message)
+            response = message_response(message)
+            client.send(response)
+            break
+
+
+if __name__ == '__main__':
+    server_main()
---
+++ Lesson 4/test_client.py
@@ -0,0 +1,47 @@
+import unittest
+import time
+import sys
+import threading
+
+sys.path.append('../Lesson 3')
+
+from client import *
+from server import *
+
+
+class TestClient(unittest.TestCase):
+    def test_create_message(self):
+        created_message = create_message()
+
+        self.assertIsInstance(created_message, bytes)
+        self.assertIn(b'action', created_message)
+        self.assertIn(b'time', created_message)
+        self.assertIn(b'user', created_message)
+
+    def test_create_message_with_account_name(self):
+        created_message = create_message('Username')
+
+        self.assertIn(b'Username', created_message)
+
+    def test_read_response(self):
+        response = {
+            "response": "200",
+            "time": f"{time.time()}",
+            "message": "OK"
+        }
+
+        encoded_response = json.dumps(response).encode('utf-8')
+
+        decoded_response = read_response(encoded_response)
+
+        self.assertEqual(decoded_response, [response['response'], response['message']])
+
+    def test_client_main(self):
+        server = threading.Thread(target=server_main)
+        server.daemon = True
+        server.start()
+        client_main()
+
+
+if __name__ == '__main__':
+    unittest.main()
