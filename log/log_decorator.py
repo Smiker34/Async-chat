c3c25b302d197a95c703e290414acc947fb333d5
Smiker34 <Smiker1@yandex.ru>
2022-12-09 22:49:21 +0300

    Update log_decorator.py

--- log/log_decorator.py
+++ log/log_decorator.py
@@ -1,21 +1,13 @@
-import logging
+from server_log_config import log
 from functools import wraps
 import inspect
 
 
-def log(func):
+def logging(func):
     @wraps(func)
     def create_log(*args, **kwargs):
         result = func(*args, **kwargs)
-
-        logs = logging.getLogger('def')
-        logs.setLevel(logging.INFO)
-        file_handler = logging.FileHandler("../log/def logs.log")
-        log_format = logging.Formatter("%(asctime)s %(message)s")
-        file_handler.setFormatter(log_format)
-        logs.addHandler(file_handler)
-
-        logs.info(f'Function {func.__name__} was called from {inspect.stack()[1][3]}')
-        logs.info(f'Function {func.__name__}({args}, {kwargs}), return {result}')
+        log.info(f'Function {func.__name__} was called from {inspect.stack()[1][3]}')
+        log.info(f'Function {func.__name__}({args}, {kwargs}), return {result}')
         return result
     return create_log
